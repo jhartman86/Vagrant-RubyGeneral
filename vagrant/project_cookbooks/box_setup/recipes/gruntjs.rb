f71b01dac5d97de0030441cec8ce28a674a776dc
Jonathan Hartman <jhartman86@gmail.com>
2014-03-14 18:30:49 -0600

    Easier setup with NPM (auto-install grunt, bower, and optionally all npm packages)

--- vagrant/project_cookbooks/box_setup/recipes/gruntjs.rb
+++
@@ -1,9 +0,0 @@
-# If nodejs is enabled, install GruntJS?
-if  node[:box_setup][:nodejs][:install] && node[:box_setup][:nodejs][:gruntjs]
-  execute "Installing GruntJS via NPM" do
-    cwd		'/home/vagrant/app'
-    user	'root'
-    command	'/usr/local/bin/npm install; /usr/local/bin/npm install -g grunt-cli'
-    action	:run
-  end
-end
